11c11f1705a40624fdf34b7cf7dcfaf63f1d1d68
Akira Komamura <akira.komamura@gmail.com>
2022-03-06 13:41:50 +0900

    chore: Add nomake linting

---
+++ .nomake/flake.nix
@@ -0,0 +1,49 @@
+{
+  description =
+    "THIS IS AN AUTO-GENERATED FILE. PLEASE DON'T EDIT IT MANUALLY.";
+  inputs = {
+    consult = {
+      flake = false;
+      owner = "minad";
+      repo = "consult";
+      type = "github";
+    };
+    doct = {
+      flake = false;
+      owner = "progfolio";
+      repo = "doct";
+      type = "github";
+    };
+    embark = {
+      flake = false;
+      owner = "oantolin";
+      repo = "embark";
+      type = "github";
+    };
+    org-dog = {
+      flake = false;
+      owner = "akirak";
+      repo = "org-dog";
+      type = "github";
+    };
+    org-dog-consult = {
+      flake = false;
+      owner = "akirak";
+      repo = "org-dog";
+      type = "github";
+    };
+    org-dog-embark = {
+      flake = false;
+      owner = "akirak";
+      repo = "org-dog";
+      type = "github";
+    };
+    org-dog-facade = {
+      flake = false;
+      owner = "akirak";
+      repo = "org-dog";
+      type = "github";
+    };
+  };
+  outputs = { ... }: { };
+}
---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "org-dog";
+
+  inputs = {
+    gnu-elpa = {
+      url = "git+https://git.savannah.gnu.org/git/emacs/elpa.git?ref=main";
+      flake = false;
+    };
+    melpa = {
+      # Temporarily use my personal branch
+      url = "github:akirak/melpa/akirak";
+      flake = false;
+    };
+
+    nomake = {
+      url = "github:emacs-twist/nomake";
+      inputs.gnu-elpa.follows = "gnu-elpa";
+      inputs.melpa.follows = "melpa";
+    };
+  };
+
+  outputs =
+    { self
+    , nomake
+    , ...
+    } @ inputs:
+    nomake.lib.mkFlake {
+      src = ./.;
+      localPackages = [
+        "org-dog"
+        "org-dog-embark"
+        "org-dog-consult"
+        "org-dog-facade"
+      ];
+    };
+}
