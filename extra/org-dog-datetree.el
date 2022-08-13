36bd435c5993026f81bb7e971dff3cd079ffd31f
Akira Komamura <akira.komamura@gmail.com>
2022-08-13 15:02:17 +0900

    fix(datetree): Change from OR match to AND match

--- extra/org-dog-datetree.el
+++ extra/org-dog-datetree.el
@@ -136,7 +136,12 @@ relevant files when an entry is archived."
                         (org-dog-select-files
                          (org-dog-make-file-pred
                           :buffer-pred
-                          `(cl-intersection ',tags org-file-tags :test #'equal)))
+                          ;; The source entry must contain all of the file tags
+                          ;; of the destination file.
+                          `(and org-file-tags
+                                (seq-every (lambda (tag)
+                                             (member tag ',tags))
+                                           org-file-tags))))
                         (cl-remove-if-not `(lambda (obj)
                                              (and (equal (oref obj root)
                                                          ,root)
