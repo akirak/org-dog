a4876e5d9cb674ae178ea3902ac090db8ab2fb1a
Akira Komamura <akira.komamura@gmail.com>
2023-05-06 15:49:12 +0900

    feat: Allow customization of the tabulated-list format

--- org-dog-tablist.el
+++ org-dog-tablist.el
@@ -8,6 +8,30 @@
     (define-key map "g" #'tabulated-list-revert)
     map))
 
+(defcustom org-dog-tablist-columns
+  '((relative
+     ("Relative" 36 t))
+    (root
+     ("Repo" 12 t))
+    (org-dog-tablist--format-class
+     ("Class" 16 t))
+    (org-dog-tablist--format-agenda-p
+     ("Agenda" 3 t))
+    (org-dog-tablist--format-file-tags
+     ("File tags" 9 t))
+    (org-dog-tablist--format-inactive-ts
+     ("Activity" 10 t)))
+  "List of columns in the tabulated list."
+  :type '(repeat (list (choice (symbol :tag "Name of the slot in the object")
+                               (function :tag "Function to return a value"))
+                       (list :tag "Format" string number boolean))))
+
+(defcustom org-dog-tablist-sort-key '("Relative" . nil)
+  "Sort key."
+  :type '(cons string boolean))
+
+(defvar org-dog-tablist--agenda-files nil)
+
 ;;;###autoload
 (defun org-dog-tablist-files ()
   "Browse org-dog files in a tabulated list interface."
@@ -22,45 +46,56 @@
 (define-derived-mode org-dog-tablist-files-mode tabulated-list-mode
   "DogTablistFiles"
   "Major mode for displaying a list of files."
-  (setq tabulated-list-format [("Relative" 36 t)
-                               ("Repo" 12 t)
-                               ("Class" 16 t)
-                               ("Agenda" 3 t)
-                               ("File tags" 9 t)
-                               ("Activity" 10 t)])
+  (setq tabulated-list-format (apply #'vector
+                                     (mapcar #'cadr org-dog-tablist-columns)))
   (setq tabulated-list-padding 2)
   ;; I am not sure what would be the best here, so I may change this later.
-  (setq tabulated-list-sort-key '("Relative" . nil))
+  (setq tabulated-list-sort-key org-dog-tablist-sort-key)
   (add-hook 'tabulated-list-revert-hook #'org-dog-tablist-files-refresh nil t)
   (tabulated-list-init-header))
 
 (defun org-dog-tablist-files-refresh ()
-  (let ((agenda-files (thread-last
-                        (org-agenda-files)
-                        (mapcar #'abbreviate-file-name))))
-    (setq tabulated-list-entries
-          (thread-last
-            (org-dog-select-files)
-            (mapcar (lambda (obj)
-                      (list (oref obj absolute)
-                            (vector (oref obj relative)
-                                    (oref obj root)
-                                    (org-dog--format-class
-                                     (eieio-object-class obj))
-                                    (if (member (oref obj absolute)
-                                                agenda-files)
-                                        "Yes"
-                                      "No")
-                                    (with-current-buffer
-                                        (org-dog-file-buffer obj)
-                                      (string-join org-file-tags ","))
-                                    (with-current-buffer
-                                        (org-dog-file-buffer obj)
-                                      (org-with-wide-buffer
-                                       (goto-char (point-min))
-                                       (if-let (ts (org-dog--latest-inactive-ts))
-                                           (ts-format "%F" ts)
-                                         "")))))))))))
+  (setq org-dog-tablist--agenda-files
+        (thread-last
+          (org-agenda-files)
+          (mapcar #'abbreviate-file-name)))
+  (setq tabulated-list-entries
+        (thread-last
+          (org-dog-select-files)
+          (mapcar (lambda (obj)
+                    (list (oref obj absolute)
+                          (thread-last
+                            org-dog-tablist-columns
+                            (mapcar #'car)
+                            (mapcar `(lambda (key)
+                                       (or (if (functionp key)
+                                               (funcall key ,obj)
+                                             (slot-value ,obj key))
+                                           "")))
+                            (apply #'vector))))))))
+
+;;;; Functions for formatting a column
+
+(defun org-dog-tablist--format-class (obj)
+  (org-dog--format-class
+   (eieio-object-class obj)))
+
+(defun org-dog-tablist--format-agenda-p (obj)
+  (if (member (oref obj absolute)
+              org-dog-tablist--agenda-files)
+      "Yes"
+    "No"))
+
+(defun org-dog-tablist--format-file-tags (obj)
+  (string-join (org-dog-file-tags obj) ","))
+
+(defun org-dog-tablist--format-inactive-ts (obj)
+  (with-current-buffer (org-dog-file-buffer obj)
+    (org-with-wide-buffer
+     (goto-char (point-min))
+     (if-let (ts (org-dog--latest-inactive-ts))
+         (ts-format "%F" ts)
+       ""))))
 
 (provide 'org-dog-tablist)
 ;;; org-dog-tablist.el ends here
