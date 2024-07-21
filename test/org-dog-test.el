c8e65634d1e0c9fcfc757a3d11d1bc726ac1f08b
Akira Komamura <akira.komamura@gmail.com>
2024-07-21 14:43:54 +0900

    test: Remove tests on obsolete functions

--- test/org-dog-test.el
+++ test/org-dog-test.el
@@ -11,21 +11,6 @@
 (defun org-dog-test-file (file)
   (expand-file-name file org-dog-test-directory))
 
-;;;; Contexts
-
-(describe "org-dog-major-mode-context-1"
-  (it "returns an `org-dog-context' object"
-    (expect (org-dog-context-p (org-dog-major-mode-context-1 'emacs-lisp-mode))
-            :to-be-truthy))
-
-  (it "returns nil on fundamental-mode"
-    (expect (org-dog-major-mode-context-1 'fundamental-mode)
-            :to-be nil))
-
-  (it "returns nil on a major mode inheriting special-mode"
-    (expect (org-dog-major-mode-context-1 'tabulated-list-mode)
-            :to-be nil)))
-
 ;;;; Utilities
 
 (describe "org-dog-inactive-ts-regexp"
