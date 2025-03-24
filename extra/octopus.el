01134be20357159bb15ef807ebdfea8d6c45ed0c
Akira Komamura <akira.komamura@gmail.com>
2025-03-24 21:36:45 +0900

    chore: Update the copyright year

--- extra/octopus.el
+++ extra/octopus.el
@@ -1,6 +1,6 @@
 ;;; octopus.el --- Transient commands for Org Dog -*- lexical-binding: t -*-
 
-;; Copyright (C) 2022 Akira Komamura
+;; Copyright (C) 2022-2025 Akira Komamura
 
 ;; Author: Akira Komamura <akira.komamura@gmail.com>
 ;; Version: 0.1
--- org-dog.el
+++ org-dog.el
@@ -1,6 +1,6 @@
 ;;; org-dog.el --- A programmable workflow layer for Org mode -*- lexical-binding: t -*-
 
-;; Copyright (C) 2022-2024 Akira Komamura
+;; Copyright (C) 2022-2025 Akira Komamura
 
 ;; Author: Akira Komamura <akira.komamura@gmail.com>
 ;; Version: 0.1
